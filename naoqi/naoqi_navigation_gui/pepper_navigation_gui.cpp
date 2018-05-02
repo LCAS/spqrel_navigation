ae9e472b203e5cae511bee133021068a9f2f81b7
mtlazaro <maritelag@gmail.com>
2018-05-02 19:19:29 +0200

    Checking definition of PEPPER_IP in pepper planner and navigation gui

--- naoqi/naoqi_navigation_gui/pepper_navigation_gui.cpp
+++ naoqi/naoqi_navigation_gui/pepper_navigation_gui.cpp
@@ -10,11 +10,15 @@ using namespace naoqi_navigation_gui;
 namespace po = boost::program_options;
 
 int main(int argc, char **argv){
+
+  std::string pepper_ip = "";
+  if (std::getenv("PEPPER_IP") != NULL)
+    pepper_ip = std::getenv("PEPPER_IP");
   
   po::options_description description("Options");
   description.add_options()
     ("help", "Displays this help message")
-    ("pip", po::value<std::string>()->default_value(std::getenv("PEPPER_IP")), "Robot IP address.  On robot or Local Naoqi: use '127.0.0.1'.")
+    ("pip", po::value<std::string>()->default_value(pepper_ip), "Robot IP address. Set IP here or for convenience, define PEPPER_IP as environment variable. On robot or Local Naoqi: use '127.0.0.1'.")
     ("pport", po::value<int>()->default_value(9559), "Naoqi port number.")
     ("map", po::value<std::string>(), "Map used for localization in YAML format with extension.")
     ;
@@ -38,9 +42,16 @@ int main(int argc, char **argv){
     mapname =  vm["map"].as<std::string>();
   }
   
+  ////////////
+  //NAOqi session initialization
   const std::string pip = vm["pip"].as<std::string>();
   int pport = vm["pport"].as<int>();
-  
+
+  if (pip == ""){
+    std::cerr << "PEPPER_IP not defined. Please, set robot ip through program options" << std::endl;
+    exit(0);
+  }
+    
   std::string tcp_url("tcp://"+pip+":"+std::to_string(pport));
 
   qi::ApplicationSession app(argc, argv, 0, tcp_url);
